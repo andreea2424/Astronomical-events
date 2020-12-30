947e59bc49f3f49c3a24d204a6b91b1c1f500573
andreea2424 <63095011+andreea2424@users.noreply.github.com>
2020-12-30 12:30:06 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,215 +3,244 @@
 using namespace std;
 
 class constellations{
-public:
-
-    char *name;
-    char *hemisphere;
-    char *form;
+private:
+    string hemisphere;
+    string form;
+    string name;
 
     constellations(){
-        name = new char[50];
-        strcpy(name, "necunoscut");
-        form = new char[50];
-        strcpy(form, "necunoscuta");
-        hemisphere = new char[50];
-        strcpy(hemisphere, "necunoscuta");
-
-    }
-
-    constellations(char *nameC, char *hemisphereC, char *formC )
-    {
-        name = new char[strlen(nameC)+1];
-        strcpy(name,nameC);
-        hemisphere = new char[strlen(hemisphereC)+1];
-        strcpy(hemisphere, hemisphereC);
-        form = new char[strlen(formC)+1];
-        strcpy(form ,formC );
-    }
-    constellations(const constellations &Cygnus) {
-
-        name = new char[strlen(Cygnus.name)+1];
-        strcpy(name,Cygnus.name);
-        form = new char[strlen(Cygnus.form) + 1];
-        strcpy(form, Cygnus.form);
-        hemisphere= new char[strlen(Cygnus.hemisphere) + 1];
-        strcpy(hemisphere, Cygnus.hemisphere);
-    }
-    string gethemisfera(){
-        return hemisphere;
-    }
-    string getform(){
-        return form;
-    }
-    ~constellations(){
-        delete []hemisphere;
-        delete []name;
-        delete []form;
-
+        this->name = name;
+        this->form = form;
+        this->hemisphere = hemisphere;
     }
 public:
-    void  constellationInformation(){
-        cout <<"The name constellation: ";
-        cout <<name << endl;
-        cout << "The shape of the constellation: ";
-        cout <<form<< endl;
-        cout <<"The hemisphere in which the constellation is located: ";
-        cout <<hemisphere<< endl;
+    constellations(string name, string hemisphere, string form )
+    {
+        this->name = name;
+        this->form = form;
+        this->hemisphere = hemisphere;
     }
+public:
+    void constellationsInform (ostream& os)
+    {
+        os <<"The name constellation: ";
+        os <<name << endl;
+        os << "The shape of the constellation: ";
+        os <<form<< endl;
+        os <<"The hemisphere in which the constellation is located: ";
+        os <<hemisphere<< endl;
+    }
+    friend ostream& operator<<(ostream& os, const constellations& );
 };
-
-
-class planet
-        {
-public:
-    char *name;
-    int nrSatellites;
-    float circumference;
-    float gravity;
-
-    planet(){
-
-        name = new char[50];
-        strcpy(name, "necunoscut");
-        nrSatellites = 0;
-        circumference = 0;
-        gravity = 0;
-        //nrPlanet = 0;
-    }
-
-    planet(char *nameP, int nrSatellites, float circumference, float gravity)
-    {
-        name = new char[strlen(nameP)+1];
-        strcpy(name,nameP);
-        this->nrSatellites= nrSatellites;
-        this->circumference=circumference;
-        this->gravity=gravity;
-
-    }
-
-  planet (const planet &plnt)
-   {
-       name = new char[strlen(plnt.name)+1];
-       strcpy(name,plnt.name);
-       nrSatellites = plnt.nrSatellites;
-       circumference = plnt.circumference;
-       gravity= plnt.gravity;
-
-   }
-
-   virtual ~planet(){
-
-        delete []name;
-    }
-
-   int getnrSattelites(){
-        return nrSatellites;
-    }
-    float getcircircumference(){
-        return circumference;
-    }
-    float getgravity(){
-        return gravity;
-    }
-
-
-public :
-    void printPlanet(){
-
-        cout << endl;
-        cout <<"Name: "<<name<<"\n";
-        cout <<"Number of sattelits: "<<nrSatellites<<"\n";
-        cout <<"Gravity of the planet in: "<<gravity<<" m/s2 "<<"\n";
-        cout<<"Circumference: "<<circumference<<" km"<<"\n";
-        cout << endl;
-
-        }
-public:
-
-    void addSattelits(){
-        nrSatellites++;
-    }
-    };
+ostream& operator<<(ostream& os, constellations& cst)
+{
+    cst.constellationsInform(os);
+    return os;
+}
 
 class astronomicalPhenomenon {
-public:
-    char *namePhenomenon;
-    char *duration;
-    char *date;
+
+    string namePhenomenon;
+    string duration;
+    string date;
 
     astronomicalPhenomenon(){
-        namePhenomenon = new char[50];
-        strcpy(namePhenomenon, "necunoscut");
-        duration = new char[50];
-        strcpy(duration, "necunoscuta");
-        date = new char[50];
-        strcpy(date, "necunoscuta");
-
+        namePhenomenon =  "unknown";
+        duration =  "unknown";
+        date =  "unknown";
     }
-
-    astronomicalPhenomenon(char *nameA,char *durationA, char *dateA )
-    {
-        namePhenomenon = new char[strlen(nameA)+1];
-        strcpy(namePhenomenon,nameA);
-        duration = new char[strlen(durationA)+1];
-        strcpy(duration, durationA);
-        date= new char[strlen(dateA)+1];
-        strcpy(date ,dateA );
-    }
-
-   astronomicalPhenomenon( astronomicalPhenomenon &Eclipse) {
-       namePhenomenon = new char[strlen(Eclipse.namePhenomenon)+1];
-       strcpy(namePhenomenon,Eclipse.namePhenomenon);
-       duration = new char[strlen(Eclipse.duration)+1];
-       strcpy(duration,Eclipse.duration);
-       date = new char[strlen(Eclipse.date)+1];
-       strcpy(date,Eclipse.date);
-
-   }
-
-
 public:
-  string getdate(){
+    astronomicalPhenomenon(string namePhenomenon,string duration,string date )
+    {
+        this->namePhenomenon =namePhenomenon;
+        this->duration =duration;
+        this->date= date;
+
+    }
+
+
+    string getdate(){
         return date ;
     }
-public:
+
     string getduartion(){
         return duration ;
     }
-    ~astronomicalPhenomenon(){
-        delete []namePhenomenon;
-        delete []duration;
-        delete []date;
+
+    void afisare_data()
+    {
+        cout << date;
     }
-public :
-    void printastronomicalPhenomenon(){
-        cout <<endl;
-        cout <<"Name: "<<namePhenomenon<<endl;
-        cout <<"Duration: "<<duration<<endl;
-        cout <<"Date: "<<date<<endl;
-        cout <<endl;
+    void printastronomicalPhenomenon(ostream& os) {
+        os << endl;
+        os << "Name: " << namePhenomenon << endl;
+        os << "Duration: " << duration << endl;
+        os << "Date: " << date << endl;
+        os << endl;
     }
+    friend ostream &operator<<(ostream &os, const astronomicalPhenomenon &);
 };
+ostream& operator<<(ostream& os, astronomicalPhenomenon& Eclipse)
+{
+    Eclipse.printastronomicalPhenomenon(os);
+    return os;
+}
+
+class star  {
+
+private:
+
+    string name;
+    float rotationSpeed;
+    double luminosity;
+    double temperature;
+
+public:
+    star(string name, double temperature, float rotationspeed,
+         double luminosity) {
+    this-> name = name;
+    this-> rotationSpeed= rotationspeed;
+    this-> luminosity = luminosity;
+    this->temperature = temperature;
+    }
+
+public:
+
+    void InfStar (ostream& os) {
+        os << endl;
+        os << "Name: " << name << endl;
+        os <<"Rostion speed: "<< rotationSpeed<<"km/s"<< endl;
+        os << "Luminosity: " << luminosity <<"L☉" <<endl;
+        os << "Temperature: "<<temperature<<"K"<< endl;
+        os << endl;
+    }
+    friend ostream &operator<<(ostream &os, const star &);
+};
+ostream& operator<<(ostream& os, star& star1)
+{
+    star1.InfStar(os);
+    return os;
+}
+
+
+
+class satellite
+{
+private:
+    string name;
+    float gravity;
+
+public:
+    satellite(string name, double temperature1, float gravity)
+     {
+        this->name = name;
+        this->gravity= gravity;
+        this->gravity=gravity;
+
+    }
+public:
+    void printsatelitte(ostream& os) {
+        os << endl;
+        os << "Name: " << name << endl;
+        os << "Temperature: " << gravity << endl;
+        os << endl;
+    }
+    friend ostream &operator<<(ostream &os, const satellite &);
+};
+ostream& operator<<(ostream& os, satellite& Stl)
+{
+    Stl.printsatelitte(os);
+    return os;
+}
+class planet {
+    satellite *satellitePlanet;
+    string name;
+    int nrSatellites;
+    float gravity;
+
+    planet(string name, float mass, double circumference, double temperature)  {
+
+        name = "unknown";
+        nrSatellites = 0;
+        gravity = 0;
+
+    }
+
+public:
+    planet(string name, int nrSatellites ,float gravity)
+   {
+        this->name = name;
+        this->nrSatellites = nrSatellites;
+        this->gravity = gravity;
+
+    }
+
+    planet(string name, int nrSatellites ,float gravity, satellite *satellitePlanet)
+            {
+        this->name = name;
+        this->nrSatellites = nrSatellites;
+        this->gravity = gravity;
+        this->satellitePlanet = satellitePlanet;
+
+    }
+
+    void observe (satellite *satellitePlanet ){
+    }
+
+    int getnrSattelites() {
+        return nrSatellites;
+    }
+    float getgravity() {
+        return gravity;
+    }
+
+    void addSattelits() {
+        nrSatellites++;
+    }
+
+
+    void printPlanet(ostream &os) {
+
+        os << endl;
+        os << "Name: " << name << "\n";
+        os << "Number of sattelits: " << nrSatellites << "\n";
+        os << "Gravity of the planet in: " << gravity << " m/s2 " << "\n";
+
+        //satellitePlanet->printsatelitte();
+        os << endl;
+
+    }
+    friend ostream& operator<<(ostream& os, const planet& );
+};
+ostream& operator<<(ostream& os,planet& P )
+{
+    P.printPlanet(os);
+    return os;
+}
+
 
 int main(){
+    satellite Moon ("Moon",10.921,127);
+    //cout << Moon;
 
-    planet Earth ( "Earth",1,40.075,9.807);
-    Earth.printPlanet();
+    planet Earth ( "Earth",1,9.807, &Moon);
+    cout << Earth;
 
-    planet Venus( "Venus",0,38.025,8.87);
-    Venus.printPlanet();
+    planet Venus( "Venus",0,8.87);
+    cout <<Venus;
 
-    planet Mercury( "Mercury",0,15.329,3.7);
-    Mercury.printPlanet();
+    planet Mercury( "Mercury",0,3.7);
+    cout << Mercury;
 
-    planet Mars( "Mars",2,21.344,3.711);
-    Mars.printPlanet();
+    planet Mars( "Mars",2,3.711);
+    cout << Mars;
 
     astronomicalPhenomenon Eclipse("Eclipsa de luna ","2:00:00","23:04:2020");
-    Eclipse.printastronomicalPhenomenon();
+    cout << Eclipse;
 
     constellations Cygnus("Cygnus", "Nordica","Lebada");
-    Cygnus.constellationInformation();
+    cout << Cygnus;
+
 
     return 0;
 }
